640ed6aa7b63c13874ba32f27c753577f4a42135
WojtekSasiela <wojtek.sasiela@gmail.com>
2016-05-08 00:08:02 +0200

    Dodanie  dodatkowego tekstu

--- ForLearnGitConflicts-Output/src/main/java/ForLearnGitConflicts/Main.java
+++ ForLearnGitConflicts-Output/src/main/java/ForLearnGitConflicts/Main.java
@@ -3,6 +3,7 @@ package ForLearnGitConflicts;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world! :)");
 		Plik pliczek = new Plik();
 		System.out.println(pliczek.nazwa);
 	}
