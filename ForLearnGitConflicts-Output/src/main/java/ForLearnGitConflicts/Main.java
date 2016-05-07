f7c34f25412a6e6c0d6034c66b80282bce23bcb7
WojtekSasiela <wojtek.sasiela@gmail.com>
2016-05-07 23:57:39 +0200

    Dodanie pliku

--- ForLearnGitConflicts-Output/src/main/java/ForLearnGitConflicts/Main.java
+++ ForLearnGitConflicts-Output/src/main/java/ForLearnGitConflicts/Main.java
@@ -3,7 +3,8 @@ package ForLearnGitConflicts;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Plik pliczek = new Plik();
+		System.out.println(pliczek.nazwa);
 	}
 
 }
---
+++ ForLearnGitConflicts-Output/src/main/java/ForLearnGitConflicts/Plik.java
@@ -0,0 +1,5 @@
+package ForLearnGitConflicts;
+
+public class Plik {
+	String nazwa = "Hello world!";
+}
